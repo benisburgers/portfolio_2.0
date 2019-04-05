3e52654c7108b938bc9c40bcfb243af86c17634f
Beni Bar-Gera <beni@Benis-iMac.local>
2019-04-05 18:00:49 +0200

    Create script to open and close cases - awesome

--- script.js
+++ script.js
@@ -9,11 +9,62 @@ $(document).ready(function(){
   	scrollHorizontal: false // Set to true if your website scrolls horizontal instead of vertical.
   });
 
+  var caseOpen = false;
+
+  function openCase(el) {
+    //find collapsed-content
+    var collapsedContent = el.find('.collapsed-content');
+    //get collapsed-content height
+    var collapsedContentHeight = collapsedContent.height();
+    //expand collapsed content
+    collapsedContent.css('max-height','none');
+    //measure collapsed content height
+    collapsedContentHeight = collapsedContent.height()
+    //collapse collapsed content
+    collapsedContent.css('max-height','0');
+    //get collapsed content height (jquery needs this for css transition)
+    collapsedContent.css('max-height');
+    //provide collapsed content with new max-height to allow transition
+    collapsedContent.css('max-height',collapsedContentHeight);
+    //add class 'expand' to this case
+    el.addClass('expand');
+    //define variable openCase as 'this' to allow current case to be closed by another case
+    caseOpen = el;
+  }
+
+  function closeCase(el) {
+    var collapsedContent = el.find('.collapsed-content');
+    collapsedContent.css('max-height','0');
+    el.removeClass('expand');
+    caseOpen = false;
+  }
+
+
   $(function() {
-      $('.case').on('click', function() {
-          console.log(this);
-          $(this).toggleClass('expand');
-      });
+    $('.case').on('click', function() {
+      var thisCase = $(this);
+      //check if a case is open
+      if (caseOpen) {
+        console.log('a case is open');
+        //check if 'this' case is open
+        if (thisCase[0] == caseOpen[0]) {
+          console.log('you clicked on the open case');
+          //close thisCase
+          closeCase(thisCase)
+        }
+        else {
+          console.log('you clicked on a closed case');
+          //close open Case
+          closeCase(caseOpen);
+          //open this case
+          openCase(thisCase)
+        }
+      }
+      else {
+        console.log('no case is open');
+        openCase(thisCase)
+      }
+    });
   });
 
 });
